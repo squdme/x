96395a2ec7b0e4d063f73e7a51381ea19349e811
johandraew <johan@draew.com>
2021-03-06 19:25:32 -0600

    Test form

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,7 +61,7 @@ module.exports = {
     }),
     // borderRadius: {
     //   none: '0',
-    //   sm: '0.125rem',
+    //   sm: '3.125rem',
     //   default: '0.25rem',
     //   lg: '0.5rem',
     //   full: '9999px',
